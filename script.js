1b1198f75e2c8a0840b679aac871ef19640b2954
AmanShukla00 <aman17101998@gmail.com>
2025-02-11 12:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 class Person {
   constructor(name, age) {
-    this._name = name;
-    this._age = age;
+    this.name = name;
+    this.age = age;
   }
 
   get name() {
-    return this._name;
+    return this.name;
   }
 
   get age() {
-    return this._age;
+    return this.age;
   }
 
   set age(newAge) {
-    this._age = newAge;
+    this.age = newAge;
   }
 }
 
